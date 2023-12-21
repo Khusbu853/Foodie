b25328910388f2747a7f242b66b13e495ddd951c
Khusbudev <dev.khusbu@vivahsahyog.com>
2023-12-21 21:33:20 +0530

    modified api

--- src/config.js
+++ src/config.js
@@ -1,16 +1,16 @@
 
 export const RESTAURANTS_URL =
-  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=25.5940499&lng=85.1376051&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+  "https://cors-anywhere.herokuapp.com/https://www.swiggy.com/dapi/restaurants/list/v5?lat=25.5940499&lng=85.1376051&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
   
 
-export const RESTAURANT_OFFER_URL = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=16.7049873&lng=74.24325270000001&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+export const RESTAURANT_OFFER_URL = "https://cors-anywhere.herokuapp.com/https://www.swiggy.com/dapi/restaurants/list/v5?lat=16.7049873&lng=74.24325270000001&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
 
 export const RESTAURANTS_IMG_URL =
-  "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
+  "https://cors-anywhere.herokuapp.com/https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
 
 // export const RESTAURANT_MENU_URL =
 //   "https://corsanywhere.herokuapp.com/https://www.swiggy.com/dapi/menu/v4/full?lat=22.73651&lng=75.889219&menuId=";
 export const RESTAURANT_MENU_URL =
-  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9715987&lng=77.5945627&restaurantId=";
+  "https://cors-anywhere.herokuapp.com/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9715987&lng=77.5945627&restaurantId=";
 
-export const RESTAURANT_SUGGESTIONS_URL = "https://www.swiggy.com/dapi/restaurants/search/suggest?lat=22.73651&lng=75.889219&str=piz&trackingId=undefined"
+export const RESTAURANT_SUGGESTIONS_URL = "https://cors-anywhere.herokuapp.com/https://www.swiggy.com/dapi/restaurants/search/suggest?lat=22.73651&lng=75.889219&str=piz&trackingId=undefined"
